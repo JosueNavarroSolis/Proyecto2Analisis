c9149fc390088c2a8f25289bb8a9b0ef83012739
JosueNavarroSolis <56096921+JosueNavarroSolis@users.noreply.github.com>
2021-01-04 18:22:48 -0600

    Create

---
+++ cmake-build-debug/CMakeFiles/Proyecto2Analisis.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto2Analisis.dir/main.cpp.obj"
+  "Proyecto2Analisis.exe"
+  "Proyecto2Analisis.exe.manifest"
+  "Proyecto2Analisis.pdb"
+  "libProyecto2Analisis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto2Analisis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
